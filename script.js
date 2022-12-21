f19acb19a1519ad532fb5c5c27c7b55d9af38e08
azzansm <89733998+azzansm@users.noreply.github.com>
2022-12-21 14:50:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Add your answer here
+$('.text-contents').fadeOut()
+
+$('.lesson').hover(
+	function() {
+		$(this).find('.text-contents').fadeIn();
+	},
+	function() {
+		$(this).find('.text-contents').fadeOut();
+	}
+);
